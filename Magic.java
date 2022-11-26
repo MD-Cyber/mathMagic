70f21599bd843eae9fd0694b6e28974db22bed83
md-cyber <>
2022-11-26 22:42:45 +0530

    finish magic number

--- Magic.java
+++ Magic.java
@@ -1,5 +1,18 @@
 public class Magic{
     public static void main(String[] args){
 
-    }
+        //original number
+        int myNumber = 10;
+
+        int stepOne = myNumber * myNumber;
+        int stepTwo = stepOne + myNumber;
+        int stepThree = stepTwo / myNumber;
+        int stepFour = stepThree + 17;
+        int stepFive = stepFour - myNumber;
+        int stepSix = stepFive / 6;
+
+        System.out.println(stepSix);
+        
+
+    }   
 }
